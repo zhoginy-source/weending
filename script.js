eb5cb8def3c4355923d7578fbb7ff9d48aa669a6
zhoginy-source <zhoginy@gmail.com>
2026-07-06 19:57:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,19 +17,14 @@ const nav = document.querySelector("#nav");
 const burger = document.querySelector("#burger");
 const navLinks = document.querySelectorAll(".nav__links a");
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
+if (nav && burger) {
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    nav.classList.remove("open");
+    nav?.classList.remove("open");
   });
-});
-
-const form = document.querySelector(".guest-form");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  alert("Анкета пока тестовая. Позже сюда можно подключить Google Forms или Яндекс Форму.");
 });
